ab56f0b51592ea0ead68d14c34514714d20c1c7d
cooperamyers <coopdawg1013@gmail.com>
2021-02-19 00:18:35 +0000

    Updated makeproject

--- MakeProject.sh
+++ MakeProject.sh
@@ -1,16 +1,16 @@
 
-echo $1 >> mkdir Cooper
+mkdir $1
 
-cd Cooper
+cd $1
 
 git init
 
 mkdir bin include lib share man info
 
-echo "Name" >> readme.md
+echo $1 >> readme.md
 
 touch .gitignore
 
-git add -A
+git add .
 
 git commit -m "Create initial structure."
